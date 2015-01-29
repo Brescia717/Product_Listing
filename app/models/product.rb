67e48c177af50a850562fa3f5e48b273063945f3
Paul Brescia <brescia717@gmail.com>
2015-01-29 01:33:02 -0500

    1. Added pry-rails to Gemfile 2. Generated Product scaffold 3. Added 'category' method to Product model 4. Modified the _form for Product to have select option for product.category_id 5. Added validations and associations for Category and Product.

---
+++ app/models/product.rb
@@ -0,0 +1,14 @@
+class Product < ActiveRecord::Base
+  validates :name,
+    presence: true
+  validates :price,
+    presence: true
+  validates :category_id,
+    presence: true
+
+  belongs_to :category
+
+  def category
+    Category.all.find(self.category_id).name
+  end
+end
