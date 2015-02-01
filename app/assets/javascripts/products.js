5efda081d2f2447697509383970555e7e3b4bf45
Paul Brescia <brescia717@gmail.com>
2015-01-31 23:04:23 -0500

    1. Added responders gem 2. Set up AJAX for search, pagination, and sort on views/products/index

---
+++ app/assets/javascripts/products.js
@@ -0,0 +1,16 @@
+$(function() {
+  $("#products th a, #products .pagination a").live("click", function() {
+    $.getScript(this.href);
+    return false;
+  });
+  $("#products_search input").keyup(function(){
+    $.get($("#products_search").attr("action"), $("#products_search").serialize(), null, "script");
+    return false;
+  });
+});
+// The coffee script version:
+// $ ->
+//   $('#products th a').live 'click', ->
+//     $.getScript @href
+//     false
+//   return
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
     end
     # css_class = (column == sort_column) ? "current #{sort_direction}" : nil
     direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-    link_to title, params.merge(sort: column, direction: direction, page: nil) #, {:class => css_class}
+    link_to title, params.merge(sort: column, direction: direction, page: nil), remote: true #, {:class => css_class}
   end
 end
