eb2581f28afc413e456bfbdc2970dd3112b1acd0
Paul Brescia <brescia717@gmail.com>
2015-02-03 16:11:27 -0500

    Added products to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 categories = Category.create([{ name: 'T-Shirts' }, { name: 'Hoodies' },
  [ name: 'Stickers' ]])
+
+products = Product.create([{ name: 'Gannon Hoodie', price: 25.00, category_id: 2, description: "It's badass."},
+  { name: "Link's Tunic", price: 12.00, category_id: 1, description: "Garb of the Hero of Time"},
+  { name: "Link Macbook Cover", price: 17.00, category_id: 3, description: "It's better than an apple..."}])
